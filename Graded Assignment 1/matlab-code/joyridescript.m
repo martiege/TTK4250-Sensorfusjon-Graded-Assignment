f190a2efff0d41cb3a7a6806096b37c21f647ba3
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2019-10-22 13:12:42 +0200

    Working -ish tun, still looses track

--- Graded Assignment 1/matlab-code/joyridescript.m
+++ Graded Assignment 1/matlab-code/joyridescript.m
@@ -50,7 +50,7 @@ covEllSize = sqrt(eig(cov(v')))
 meanMinMeasDists = mean(d)
 meanNumberOfCloseMeasurements = mean(closecount)
 %%
-% % play measurement movie. Remember that you can click the stop button.
+% play measurement movie. Remember that you can click the stop button.
 % figure(2); clf; grid on;
 % set(gcf,'Visible','on')
 % plotpause = 0.1; % sets a pause in between time steps if it goes to fast
@@ -70,15 +70,15 @@ meanNumberOfCloseMeasurements = mean(closecount)
 % % Look at individual EKF-PDAs
 % 
 % r = 8^2; %...
-% qCV = 3e-1; % 7e-4; % 0.14; %...
-% qCT = [1e-5, 1e-5]; % [0.0001, 0.00001]; %...
+% qCV = 3e-1; % 3e-1; % 7e-4; % 0.14; %...
+% qCT = [1e-1, 5e-3]; % [1e-4, 1e-5]; % [0.0001, 0.00001]; %...
 % 
 % 
 % PD = 0.95; %...
 % lambda = 1e-3; %...
 % gateSize = 5^2; %...
 % % choose model to tune
-% s = 1;
+% s = 2;
 % 
 % % make models
 % models =  cell(2,1);
@@ -189,35 +189,40 @@ meanNumberOfCloseMeasurements = mean(closecount)
 % IMM-PDA
 
 % sensor 
-r = 6^2; %...
-PD = 0.95; %...
+r = 8^2; %...
+PD = 0.96; %...
 lambda = 1e-3; %...
-gateSize = 5^2; %...
+gateSize = 10^2; %...
 
 % dynamic models
-qCV = 7e-4; %...
-qCT = [1e-5, 1e-5]; %...
-qCVh = 1e0; %...
+qCV = 7.8e-3; %...
+qCT = [2e-3, 5e-5]; %...
+qCVh = 5e-1; %...
 modIdx = 1:3; 
 M = numel(modIdx);
 
 x0 = [7100; 3630; 0; 0; 0]; % taken from gt
 P0 = diag([25, 25, 10, 10, pi/6].^2); % seems reasonable?
 
-% markov chain (other parameterizations can be simpler to tune)
-% PI11 = %...
-% PI22 = %...
-% PI33 = %...
 
-PI = [0.95, 0.025, 0.025; 
-      0.025, 0.95, 0.025; 
-      0.025, 0.025, 0.95]; 
+% markov chain (other parameterizations can be simpler to tune)
+% PI11 = 0.9; %...
+% PI22 = 0.99; %...
+% PI33 = 0.5; %...
+% 
+% PI = [PI11,         1 - PI22/2, 1 - PI33/2; 
+%       1 - PI11/2,   PI22,       1 - PI33/2; 
+%       1 - PI11/2,   1 - PI22/2, PI33]; 
+
+PI = [0.8696, 0.0270, 0.0370; 
+      0.0870, 0.4865, 0.2222;
+      0.0435, 0.4865, 0.7407]; 
 
 PI = PI(modIdx, modIdx) % select the models to use
 PI = PI./sum(PI,1); % be sure to normalize
 assert(all(sum(PI, 1) - 1 < eps),'columns of PI must sum to 1')
 
-sprobs0 = [1, 0.1, 1]; %... 
+sprobs0 = [1, 0.1, 0.1]; %... 
 sprobs0 = sprobs0(modIdx)/sum(sprobs0(modIdx)) % select models and normalize
 assert(all(sprobs0 > 0), 'probabilities must be positive')
 
@@ -251,7 +256,7 @@ probbar(:, 1) = sprobs0;
 % filter
 for k=1:K
     [probhat(:, k), xhat(:, :, k), Phat(:, :, :, k)] = tracker.update(Z{k}, probbar(:, k), xbar(:, :, k), Pbar(:, :, :, k)); %... update
-    [xest(:, k), Pest(:, :, k)] = reduceGaussMix(probhat(:, k), xhat(:, :, k), Phat(:, :, :, k)); %...
+    [xest(:, k), Pest(:, :, k)] = tracker.imm.estimate(probhat(:, k), xhat(:, :, k), Phat(:, :, :, k)); %...
     
     NEES(k) = ((xest(1:4, k) - Xgt(:, k))' / Pest(1:4, 1:4, k)) * (xest(1:4, k) - Xgt(:, k)); % ... 
     NEESpos(k) = ((xest(1:2, k) - Xgt(1:2, k))'/  squeeze(Pest(1:2,1:2 ,k))) * (xest(1:2, k) - Xgt(1:2, k)); % ...
@@ -340,70 +345,73 @@ inCI = sum((NEESvel >= ciNEES(1)) .* (NEESvel <= ciNEES(2)))/K * 100;
 plot([1,K], repmat(ciNEES',[1,2])','r--')
 text(K*1.04, -5, sprintf('%.2f%% inside CI', inCI),'Rotation',90);
 %%
-% %estimation "movie"
-% mTL = 0.2; % maximum transparancy (between 0 and 1);
-% plotpause = 1; % lenght to pause between time steps;
-% plotRange = 2:K; % the range to go through
-% N = 50; % number of points to use for ellipse;
-% 
-% %k = 31; assert(all([k > 1, k <= K]), 'K must be in proper range')
-% figure(11); clf; grid on; hold on; axis equal;
-% set(gcf,'Visible','on')
-% co = get(gca, 'colorOrder');
-% minAx = zeros(1,2);
-% maxAx = 7000*ones(1,2);
-% thetas = (0:N)*2*pi/N;
-% for k = plotRange
-%     % tracker
-%     subplot(1,2,1);
-%     cla; grid on; hold on; axis equal;
-%     title(sprintf('time step %d', k));
-%     gated = tracker.gate(Z{k}, probbar(:, k), xbar(:, :, k), Pbar(:, :, :, k));
-%     minG = 1e20 * ones(2,1);
-%     maxG = -1e20 * ones(2,1);
-%     [skupd, xkupd, Pkupd] = tracker.conditionalUpdate(Z{k}(:, gated), probbar(:, k), xbar(:, :, k), Pbar(:, :, :, k));
-%     beta = tracker.associationProbabilities(Z{k}(:, gated), probbar(:, k), xbar(:, :, k), Pbar(:, :, :, k));
-%     for s = 1:M
-%         plot(squeeze(xhat(1, s, 1:(k-1))), squeeze(xhat(2, s, 1:(k-1)))','Color', co(s,:));
-%         axis([minAx(1), maxAx(1), minAx(2), maxAx(2)])
-%         for j = 1:size(xkupd, 3)
-%             csj = mTL * co(s, :) + (1 - mTL) * (beta(j)*skupd(s, j)*co(s, :) + (1 - beta(j)*skupd(s, j)) * ones(1, 3)); % transparancy
-%             pl = plot([squeeze(xhat(1, s, k-1)); xkupd(1, s, j)], [squeeze(xhat(2, s, k-1)); xkupd(2, s, j)], '--', 'Color', csj);
-%             axis([minAx(1), maxAx(1), minAx(2), maxAx(2)])
-%             %alpha(pl, beta(j)*skupd(s, j));
-%             drawnow;
-%             covEllData = chol(Pkupd(1:2,1:2, s, j))' * [cos(thetas); sin(thetas)] + xkupd(1:2, s, j);
-%             pl = plot(covEllData(1, :), covEllData(2, :), '--', 'Color', csj);
-%         end
-%         
-%         [vk, Sk] = imm.modeFilters{s}.innovation([0,0], squeeze(xbar(:, s, k)), squeeze(Pbar(:, :, s, k)));
-%         gateData = chol(Sk)' * [cos(thetas); sin(thetas)] * sqrt(tracker.gateSize) + squeeze(xbar(1:2, s, k));
-%         plot(gateData(1, :),gateData(2, :), '.--', 'Color', co(s,:))
-%         scatter(Z{k}(1, :), Z{k}(2, :), 'rx')
-%         
-%         minGs = min(gateData, [], 2);
-%         minG = minGs .* (minGs < minG) + minG .* (minG <= minGs);
-%         maxGs = max(gateData, [], 2);
-%         maxG = maxGs .* (maxGs > maxG) + maxG .* (maxG >= maxGs);
-%         
-%     end
-%     scale = 1;
-%     minAx = minG - scale * (maxG - minG);
-%     maxAx = maxG + scale * (maxG - minG);
-%     axis([minAx(1), maxAx(1), minAx(2), maxAx(2)])
-%     %legend()
-%     
-%     % mode probabilities
-%     subplot(1,2,2)
-%     cla; grid on; hold on;
-%     for s = 1:M
-%         plot(probhat(s, 1:(k - 1)), 'Color', co(s, :));
-%         for j = 1:size(xkupd, 3)
-%             csj = mTL * co(s, :) + (1 - mTL) * (beta(j)*skupd(s, j)*co(s, :) + (1 - beta(j)*skupd(s, j)) * ones(1, 3)); % transparancy
-%             plot([k-1, k], [probhat(s, k-1), skupd(s, j)], '--', 'color', csj)
-%         end
-%     end
-%     axis([1, plotRange(end), 0, 1])
-%     drawnow;
-%     pause(plotpause)
-% end
+%estimation "movie"
+play_estimation_movie = 0;
+if play_estimation_movie
+    mTL = 0.2; % maximum transparancy (between 0 and 1);
+    plotpause = 1; % lenght to pause between time steps;
+    plotRange = 10:30; % 2:K; % the range to go through
+    N = 50; % number of points to use for ellipse;
+
+    %k = 31; assert(all([k > 1, k <= K]), 'K must be in proper range')
+    figure(11); clf; grid on; hold on; axis equal;
+    set(gcf,'Visible','on')
+    co = get(gca, 'colorOrder');
+    minAx = zeros(1,2);
+    maxAx = 7000*ones(1,2);
+    thetas = (0:N)*2*pi/N;
+    for k = plotRange
+        % tracker
+        subplot(1,2,1);
+        cla; grid on; hold on; axis equal;
+        title(sprintf('time step %d', k));
+        gated = tracker.gate(Z{k}, probbar(:, k), xbar(:, :, k), Pbar(:, :, :, k));
+        minG = 1e20 * ones(2,1);
+        maxG = -1e20 * ones(2,1);
+        [skupd, xkupd, Pkupd] = tracker.conditionalUpdate(Z{k}(:, gated), probbar(:, k), xbar(:, :, k), Pbar(:, :, :, k));
+        beta = tracker.associationProbabilities(Z{k}(:, gated), probbar(:, k), xbar(:, :, k), Pbar(:, :, :, k));
+        for s = 1:M
+            plot(squeeze(xhat(1, s, 1:(k-1))), squeeze(xhat(2, s, 1:(k-1)))','Color', co(s,:));
+            axis([minAx(1), maxAx(1), minAx(2), maxAx(2)])
+            for j = 1:size(xkupd, 3)
+                csj = mTL * co(s, :) + (1 - mTL) * (beta(j)*skupd(s, j)*co(s, :) + (1 - beta(j)*skupd(s, j)) * ones(1, 3)); % transparancy
+                pl = plot([squeeze(xhat(1, s, k-1)); xkupd(1, s, j)], [squeeze(xhat(2, s, k-1)); xkupd(2, s, j)], '--', 'Color', csj);
+                axis([minAx(1), maxAx(1), minAx(2), maxAx(2)])
+                %alpha(pl, beta(j)*skupd(s, j));
+                drawnow;
+                covEllData = chol(Pkupd(1:2,1:2, s, j))' * [cos(thetas); sin(thetas)] + xkupd(1:2, s, j);
+                pl = plot(covEllData(1, :), covEllData(2, :), '--', 'Color', csj);
+            end
+
+            [vk, Sk] = imm.modeFilters{s}.innovation([0,0], squeeze(xbar(:, s, k)), squeeze(Pbar(:, :, s, k)));
+            gateData = chol(Sk)' * [cos(thetas); sin(thetas)] * sqrt(tracker.gateSize) + squeeze(xbar(1:2, s, k));
+            plot(gateData(1, :),gateData(2, :), '.--', 'Color', co(s,:))
+            scatter(Z{k}(1, :), Z{k}(2, :), 'rx')
+
+            minGs = min(gateData, [], 2);
+            minG = minGs .* (minGs < minG) + minG .* (minG <= minGs);
+            maxGs = max(gateData, [], 2);
+            maxG = maxGs .* (maxGs > maxG) + maxG .* (maxG >= maxGs);
+
+        end
+        scale = 1;
+        minAx = minG - scale * (maxG - minG);
+        maxAx = maxG + scale * (maxG - minG);
+        axis([minAx(1), maxAx(1), minAx(2), maxAx(2)])
+        %legend()
+
+        % mode probabilities
+        subplot(1,2,2)
+        cla; grid on; hold on;
+        for s = 1:M
+            plot(probhat(s, 1:(k - 1)), 'Color', co(s, :));
+            for j = 1:size(xkupd, 3)
+                csj = mTL * co(s, :) + (1 - mTL) * (beta(j)*skupd(s, j)*co(s, :) + (1 - beta(j)*skupd(s, j)) * ones(1, 3)); % transparancy
+                plot([k-1, k], [probhat(s, k-1), skupd(s, j)], '--', 'color', csj)
+            end
+        end
+        axis([1, plotRange(end), 0, 1])
+        drawnow;
+        pause(plotpause)
+    end
+end
