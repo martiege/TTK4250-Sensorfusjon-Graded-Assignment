46bba2f9f85349ab38f99fc3e594dff517502ea2
Martin Eek Gerhardsen <martiege@stud.ntnu.no>
2019-11-22 11:44:05 +0100

    Innlevering

---
+++ Graded Assignment 3/matlab-code/innlevering/EKFSLAM.m
@@ -0,0 +1,274 @@
+classdef EKFSLAM
+    properties
+        Q
+        R
+        doAsso
+        alpha
+        sensOffset
+        doChecks
+    end
+    methods
+        function obj = EKFSLAM(Q, R, doAsso, alphas, sensorOffset, doChecks)
+            obj.Q = Q;
+            obj.R = R;
+            
+            if nargin < 3
+                doAsso = false;
+            end
+            obj.doAsso = doAsso;
+            
+            if nargin < 4
+                alphas = [0.001, 0.0001];
+            end
+            obj.alpha = alphas;
+            
+            if nargin < 5
+                sensorOffset = zeros(2,1);
+            end
+            
+            if nargin < 6
+                doChecks = false; 
+            end
+            obj.doChecks = doChecks; 
+            
+            obj.sensOffset = sensorOffset(:);
+        end
+        
+        function xpred = f(~, x, u)
+            % equation 11.7
+            s_psi = sin(x(3)); 
+            c_psi = cos(x(3)); 
+            xpred = [x(1) + u(1) * c_psi - u(2) * s_psi;
+                     x(2) + u(1) * s_psi + u(2) * c_psi;
+                     x(3) + u(3)];
+        end
+        
+        function Fx = Fx(obj, x, u)
+            % equation 11.13
+            s_psi = sin(x(3)); 
+            c_psi = cos(x(3));
+            Fx = [1, 0, -u(1) * s_psi - u(2) * c_psi; 
+                  0, 1,  u(1) * c_psi - u(2) * s_psi; 
+                  0, 0,  1]; 
+            
+            % Check that jacobian is correct, remove for speed
+            if obj.doChecks
+                if norm(Fx - jacobianFD(@(X) obj.f(X, u), x, 1e-5), 'fro') > 1e-3
+                    error('some error in pred Jac')
+                end
+            end
+        end
+        
+        function Fu = Fu(obj, x, u)
+            % equation 11.14
+            s_psi = sin(x(3)); 
+            c_psi = cos(x(3));
+            Fu = [c_psi, -s_psi, 0; 
+                  s_psi,  c_psi, 0; 
+                      0,      0, 1]; 
+              
+            % Check that the jacobian is correct, remove for speed
+            if obj.doChecks
+                if norm(Fu - jacobianFD(@(U) obj.f(x, U), u, 1e-5), 'fro') > 1e-3
+                    jacobianFD(@(U) obj.f(x, U), u, 1e-5)
+                    error('some error in pred Jac')
+                end
+            end
+        end
+        
+        function [etapred, P] =  predict(obj, eta, P, zOdo)
+            x = eta(1:3); % pose
+            m = eta(4:end); % map
+            
+            xpred = obj.f(x, zOdo);
+            Fx = obj.Fx(x, zOdo); 
+            Fu = obj.Fu(x, zOdo); 
+            
+            % Predict covariance in place for performance 
+            P(1:3, 1:3) = Fx * P(1:3, 1:3) * Fx' + Fu * obj.Q * Fu';
+            P(1:3, 4:end) = Fx * P(1:3, 4:end); 
+            P(4:end, 1:3) = P(4:end, 1:3) * Fx'; 
+            
+            % Concatenate pose and landmarks again
+            etapred = [xpred; m];
+            
+            % Check that the covariance makes sense
+            if obj.doChecks
+                if any(eig(P) <= 0) % costly, remove when tested
+                    warn('EKFpredict got cov not PSD')
+                end
+            end
+        end
+        
+        function zpred = h(obj, eta)
+            x = eta(1:3); % pose 
+            m = reshape(eta(4:end), 2 ,[]); % map (2 x m now)
+            
+            Rot = rotmat2d(-x(3)); % Rotation matrix from world to body
+            
+            % Cartesian measurement in world:
+            z_c = m - x(1:2) - Rot' * obj.sensOffset;
+            
+            % Measurement in body:
+            z_b = Rot * z_c; 
+            
+            % Do polar coordinate transformation:
+            zpred = [sqrt(sum(z_b.^2, 1)); atan2(z_b(2, :), z_b(1, :))]; 
+            
+            % Make column again:
+            zpred = zpred(:); 
+        end
+        
+        function H = H(obj, eta)
+            x = eta(1:3); % pose
+            m = reshape(eta(4:end), 2 ,[]); % map
+            
+            numM = size(m, 2);  % number of landmarks
+            
+            Rot = rotmat2d(x(3));
+            
+            m_minus_rho = m - x(1:2); 
+            z_c = m_minus_rho - Rot * obj.sensOffset;
+
+            Rpihalf = [0, -1; 1, 0]; 
+            
+            % Allocate
+            Hx = zeros(2 * numM, 3); % pose columns
+            Hm = zeros(2 * numM, 2 * numM); % map columns (the rest)
+            
+            for i = 1:numM
+                inds = 2*(i - 1) + [1; 2];
+                
+                jac_z_b = [-eye(2), -Rpihalf * m_minus_rho(:, i)];
+                
+                Hx(inds(1), :) = z_c(:, i)' / norm(z_c(:, i)) * jac_z_b; % jac z_r 
+                Hx(inds(2), :) = z_c(:, i)' * Rpihalf' / (norm(z_c(:, i))^2) * jac_z_b; % jac z_phi
+            
+                Hm(inds, inds) = -Hx(inds, 1:2);
+            end
+        
+            % Concatenate the H matrix
+            H = [Hx, Hm];
+            
+            % Check that it is done correctly, remove for speed
+            if obj.doChecks
+                if norm(H - jacobianFD(@(X) obj.h(X), eta, 1e-5), 'fro') > 1e-3
+                    error('some error in meas Jac')
+                end
+            end
+        end
+        
+        function [etaadded, Padded] = addLandmarks(obj, eta, P, z)
+            n = size(P, 1);
+            numLmk = numel(z)/2;
+            
+            % Allocate
+            lmnew = zeros(size(z));
+            Gx = zeros(numLmk * 2, 3);
+            Rall = zeros(numLmk * 2, numLmk * 2);
+            
+            Rpsipluspihalf = rotmat2d(eta(3) + pi/2); 
+
+            for j = 1:numLmk
+                % Find indeces and the relevant measurement
+                inds = 2 * (j - 1) + [1, 2];
+                zj = z(inds);
+                
+                rot_meas = rotmat2d(zj(2)); 
+                rot_body = rotmat2d(eta(3));
+
+                lmnew(inds) = rot_body * (pol2cart(zj) + obj.sensOffset) + eta(1:2); % mean
+                Gx(inds, :) = [eye(2), zj(1) * [-sin(zj(2) + eta(3)); cos(zj(2) + eta(3))] + Rpsipluspihalf * obj.sensOffset]; % jac h^-1 wrt. x
+                Gz =  rot_meas * rot_body * diag([1, zj(1)]); % jac h^-1 wrt. z
+                Rall(inds, inds) = Gz * obj.R * Gz'; % the linearized measurement noise
+
+            end
+            
+            % Augment state
+            etaadded = [eta; lmnew];
+            
+            % Add covariances
+            Padded = blkdiag(P, Gx * P(1:3, 1:3) * Gx' + Rall);
+            Padded((n+1):end, 1:n) = Gx*P(1:3, :);
+            Padded(1:n, (n+1):end) = P(:, 1:3)*Gx';
+
+            % Sanity check, remove for speed
+            if obj.doChecks
+                if any(eig(Padded) <= 0) % costly, remove when tested
+                    warning('EKFupdate got cov not PSD after adding a landmark');
+                end
+            end
+        end
+        
+        function [z, zpred, H, S, a] = associate(obj, z, zpred, H, S)
+            if obj.doAsso
+                % Associate
+                a = JCBB(z, zpred, S, obj.alpha(1), obj.alpha(2));
+
+                % Extract associated measurements
+                zinds = false(size(z));
+                zinds(1:2:end) = a > 0;
+                zinds(2:2:end) = zinds(1:2:end);
+                z = z(zinds);
+
+                % Extract and rearrange predicted measurements and cov
+                zbarinds = reshape([2*a(a>0) - 1, 2*a(a>0)]', [], 1);
+                zpred = zpred(zbarinds);
+                S = S(zbarinds, zbarinds);
+                H = H(zbarinds, :);
+            end
+        end
+        
+        function [etaupd, Pupd, NIS, a] = update(obj, eta, P, z)    
+            numLmk = (numel(eta) - 3)/2; % number of landmarks
+            if numLmk > 0
+                % Prediction and innovation covariance
+                zpred = obj.h(eta);
+                H = obj.H(eta); 
+                S = H * P * H' + kron(eye(numLmk), obj.R); 
+                z = z(:); % vectorize
+                
+                % Perform data association if it is asked for
+                [za, zpred, H, S, a] = obj.associate(z, zpred, H, S);
+
+                % Create the associated innovation
+                v = za(:) - zpred;
+                v(2:2:end) = wrapToPi(v(2:2:end)); % angles are in [-pi, pi]
+
+                % Kalman update
+                W = P * H' / S; 
+                etaupd = eta + W * v; 
+                NIS = v' / S * v; 
+                Pupd = (eye(size(P)) - W * H) * P; 
+                
+                % Sanity check, remove for speed
+                if obj.doChecks
+                    if any(eig(Pupd) <= 0) % costly, remove when tested
+                        warn('EKFupdate got cov not PSD');
+                    end
+                end
+            else % all measurements are new landmarks
+                a = zeros(size(z, 2), 1);
+                z = z(:);
+                NIS = 0;
+                etaupd = eta;
+                Pupd = P;
+            end
+            
+            % Create new landmarks if any is available
+            if obj.doAsso
+                isNewLmk = (a == 0);
+                if any(isNewLmk)
+                    % Extract unassociated measurements
+                    zNewInds = false(size(z));
+                    zNewInds(1:2:end) = isNewLmk;
+                    zNewInds(2:2:end) = isNewLmk;
+                    znew = z(zNewInds);
+                    
+                    % Create new landmarks
+                    [etaupd, Pupd] = obj.addLandmarks(eta, P, znew);
+                end
+            end  
+        end
+    end
+end
---
+++ Graded Assignment 3/matlab-code/innlevering/plot_simulated_SLAM.m
@@ -0,0 +1,100 @@
+style = hgexport('factorystyle');
+style.bounds = 'tight';
+style.Format = 'eps';
+style.Width = 6;
+style.Height = 2.5;
+style.Resolution = 300;
+style.Units = 'inch';
+style.FixedFontSize = 12;
+
+% plotting
+if plot_results
+    f = figure(3);
+    k = N;
+    clf;
+    %subplot(1,2,1);
+    hold on;
+
+    scatter(landmarks(1,:), landmarks(2,:), 'r^')
+    scatter(xhat{k}(4:2:end), xhat{k}(5:2:end), 'b.')
+
+    lh1 = plot(poseGT(1, 1:k), poseGT(2,1:k), 'r', 'DisplayName', 'Ground Truth');
+    lh2 = plot(cellfun(@(x) x(1), xhat), cellfun(@(x) x(2), xhat), 'b', 'DisplayName', 'Estimated');
+
+    el = ellipse(xhat{k}(1:2),Phat{k}(1:2,1:2),5,200);
+    plot(el(1,:),el(2,:),'b');
+
+    for ii=1:((size(Phat{k}, 1)-3)/2)
+       rI = squeeze(Phat{k}(3+[1,2]+(ii-1)*2,3+[1,2]+(ii-1)*2));
+       el = ellipse(xhat{k}(3 + (1:2) + (ii-1)*2),rI,5,200);
+       plot(el(1,:),el(2,:),'b');
+    end
+
+    axis equal;
+    title('Estimated trajectory and landmarks vs. ground truth')
+    legend([lh1, lh2])
+    grid on;
+    
+    if export_plots
+        hgexport(f,'figures/ga_3_sim_trajectory.eps',style,'Format','eps');
+    end
+end
+
+%% consistency: what to do with variable measurement size..?
+alpha = 0.05;
+ANIS = mean(NIS)
+ACI = chi2inv([alpha/2; 1 - alpha/2], 1)/N % NOT CORRECT NOW
+CI = zeros(2, N); % chi2inv([alpha/2; 1 - alpha/2], 1); % NOT CORRECT NOW
+warning('These consistency intervals have wrong degrees of freedom')
+for i = 1:N
+    CI(:, i) = chi2inv([alpha/2; 1 - alpha/2], 2 * nnz(a{i})); 
+end
+
+if plot_nis
+    f = figure(5); clf;
+    hold on;
+    plot(1:N, NIS(1:N));
+    insideCI = mean((CI(1, :) < NIS) .* (NIS <= CI(2, :)))*100;
+    plot(1:N, CI(1, :),'r--');
+    plot(1:N, CI(2, :),'r--');
+    title(sprintf('NIS over time, with %0.1f%% inside %0.1f%% CI', insideCI, (1-alpha)*100));
+    grid on;
+    ylabel('NIS');
+    xlabel('time');
+    
+    if export_plots
+        hgexport(f,'figures/ga_3_sim_NIS.eps',style,'Format','eps');
+    end
+end
+
+figure(10);
+plot(RMSE);
+
+%% run a movie
+if plot_movie
+    pauseTime = 0.05;
+    fig = figure(4);
+    ax = gca;
+    for k = 1:N
+        cla(ax); hold on;
+        scatter(ax, landmarks(1,:), landmarks(2,:), 'r^')
+        scatter(ax, xhat{k}(4:2:end), xhat{k}(5:2:end), 'b*')
+        plot(ax, poseGT(1, 1:k), poseGT(2,1:k), 'r-o','markerindices',10:10:k);
+        plot(ax, cellfun(@(x) x(1), xhat(1:k)), cellfun(@(x) x(2), xhat(1:k)), 'b-o','markerindices',10:10:k);
+
+        if k > 1 % singular cov at k = 1
+            el = ellipse(xhat{k}(1:2),Phat{k}(1:2,1:2),5,200);
+            plot(ax,el(1,:),el(2,:),'b');
+        end
+
+        for ii=1:((size(Phat{k}, 1)-3)/2)
+           rI = squeeze(Phat{k}(3+[1,2]+(ii-1)*2,3+[1,2]+(ii-1)*2)); 
+           el = ellipse(xhat{k}(3 + (1:2) + (ii-1)*2),rI,5,200);
+           plot(ax, el(1,:),el(2,:),'b');
+        end
+
+        title(ax, sprintf('k = %d',k))
+        grid(ax, 'on');
+        pause(pauseTime);
+    end
+end
